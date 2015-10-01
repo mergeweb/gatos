21d2f0452a43fe25badc1fa318d0a9fadb05e930
wjbarber3 <wjbarber3@gmail.com>
2015-10-01 14:08:02 -0400

    removing commented out stuff from function.php

--- wp-content/themes/upup/functions.php
+++ wp-content/themes/upup/functions.php
@@ -22,25 +22,4 @@ if( !is_admin() ) {
 	add_action('init', 'jquery_method');
 }
 
-
-// Form
-
-
-
-// /* Replace XXX with your Gravity Form ID. e.g., gform_after_submission_2 for Form 2 */
-// add_action('gform_after_submission_1', 'add_to_google_spreadsheet', 10, 2);
- 
-// function add_to_google_spreadsheet($entry, $form) {
- 
-//     // This is the web app URL of your Google Script create in previous step
-//     $post_url = 'https://script.google.com/macros/s/AKfycbyRz9laq933aIX7YkpAG95Sf4W9jTz6e4F4D7aGMY7FdE-A5CQ/exec';
- 
-//     // Put all the form fields (names and values) in this array
-//     $body = array('input_1' => rgar($entry, '1'));
- 
-//     // Send the data to Google Spreadsheet via HTTP POST request
-//     $request = new WP_Http();
-//     $response = $request->request($post_url, array('method' => 'POST', 'sslverify' => false, 'body' => $body));
-// }
-
 ?>
